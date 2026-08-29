fce42fcfa36e09bc989921f41a3313056b3221ff
rokodinga <rokodinga@gmail.com>
2026-08-29 13:33:42 +0530

    Add profile and forgot-password routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { AuthProvider } from './firebase/auth-context';
 import { ProtectedRoute } from './components/ProtectedRoute';
 import { LoginPage } from './pages/LoginPage';
 import { SignupPage } from './pages/SignupPage';
+import { ForgotPasswordPage } from './pages/ForgotPasswordPage';
+import { ProfilePage } from './pages/ProfilePage';
 import { DashboardPage } from './pages/DashboardPage';
 import { LetterRegisterPage } from './pages/LetterRegisterPage';
 import './App.css';
@@ -14,30 +16,11 @@ function App() {
         <Routes>
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
-          <Route
-            path="/dashboard"
-            element={
-              <ProtectedRoute>
-                <DashboardPage />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/incoming"
-            element={
-              <ProtectedRoute>
-                <LetterRegisterPage type="incoming" />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/outgoing"
-            element={
-              <ProtectedRoute>
-                <LetterRegisterPage type="outgoing" />
-              </ProtectedRoute>
-            }
-          />
+          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+          <Route path="/dashboard" element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} />
+          <Route path="/profile" element={<ProtectedRoute><ProfilePage /></ProtectedRoute>} />
+          <Route path="/incoming" element={<ProtectedRoute><LetterRegisterPage type="incoming" /></ProtectedRoute>} />
+          <Route path="/outgoing" element={<ProtectedRoute><LetterRegisterPage type="outgoing" /></ProtectedRoute>} />
           <Route path="/" element={<Navigate to="/dashboard" replace />} />
           <Route path="*" element={<Navigate to="/dashboard" replace />} />
         </Routes>
