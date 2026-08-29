4ae7519933c311599e6c56d389e13164d0504193
rokodinga <rokodinga@gmail.com>
2026-08-29 13:43:56 +0530

    Add protected administrator audit route

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { SignupPage } from './pages/SignupPage';
 import { ForgotPasswordPage } from './pages/ForgotPasswordPage';
 import { ProfilePage } from './pages/ProfilePage';
 import { UserManagementPage } from './pages/UserManagementPage';
+import { AdminAuditPage } from './pages/AdminAuditPage';
 import { DashboardPage } from './pages/DashboardPage';
 import { LetterRegisterPage } from './pages/LetterRegisterPage';
 import './App.css';
@@ -22,6 +23,7 @@ function App() {
           <Route path="/dashboard" element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} />
           <Route path="/profile" element={<ProtectedRoute><ProfilePage /></ProtectedRoute>} />
           <Route path="/admin/users" element={<AdminRoute><UserManagementPage /></AdminRoute>} />
+          <Route path="/admin/audit" element={<AdminRoute><AdminAuditPage /></AdminRoute>} />
           <Route path="/incoming" element={<ProtectedRoute><LetterRegisterPage type="incoming" /></ProtectedRoute>} />
           <Route path="/outgoing" element={<ProtectedRoute><LetterRegisterPage type="outgoing" /></ProtectedRoute>} />
           <Route path="/" element={<Navigate to="/dashboard" replace />} />
