120a405284b5b6c0e2714105134923462c5221ff
rokodinga <rokodinga@gmail.com>
2026-08-31 18:54:59 +0530

    Fix Gmail sync API handler loop closure

--- api/admin/gmail.js
+++ api/admin/gmail.js
@@ -510,6 +510,7 @@ export default async function handler(req, res) {
               : {}),
           }, { merge: true });
         }
+      }
 
       return res.status(200).json({
         ok: errors.length === 0,
